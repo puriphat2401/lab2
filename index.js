85e0dc44489b242b151c2f99e0604a9d3a3b816e
puriphat2401 <684244135@mail.pbru.ac.th>
2025-10-30 15:00:20 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+constdisla =document.getElementById("displa");
+
+
+function appendtoDig(input){
+
+}
+
+
+function appendtoDig(input){
+    displa.value = input;
+}
+
+function caicuator(){
+try{
+    display.value = eval(displa.value);
+}
+catch(error){
+    displa.value ="Error";
+}
+}
